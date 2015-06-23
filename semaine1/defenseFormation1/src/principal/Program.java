97bd8bb0cd7826f0046d7a741badb522ed0bc847
messan55 <messan.gnemegna@gmail.com>
2015-06-23 16:55:27 +0200

    test

---
+++ semaine1/defenseFormation1/src/principal/Program.java
@@ -0,0 +1,15 @@
+package principal;
+
+public class Program {
+	public static void main(String[] args){
+		System.out.println("bonjour monde");
+		System.out.println("et bon appetit");
+		
+		int i = 14;
+		int j = 15;
+		long l = 12631616;
+		l = i;
+		System.out.println(i+j);
+	}
+
+}
